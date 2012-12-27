05d9c394378d131677cf921a93d0660d27b27481
Elmer He <widgetrefinery+0ea3de46@gmail.com>
2012-12-26 20:13:57 -0800

    updated router

--- ui/src/js/router.js
+++ ui/src/js/router.js
@@ -1,19 +1,26 @@
-define(['sammy', 'util/app'], function (sammy, app) {
+define([
+	'sammy',
+	'util/uri'
+], function (sammy, URI) {
+
 	var prevPage;
 
 	var loadPage = function (pageName) {
 		return function () {
 			require([pageName], function (page) {
+				URI.current(window.location.hash);
 				prevPage = new page(prevPage);
 			});
 		};
 	};
 
 	var router = sammy(function () {
-		this.get('#account', loadPage('page/accounts'));
+		this.get('#account', loadPage('page/accountList'));
 		this.get('#account/:id', loadPage('page/account'));
 		this.get('#account/:id/edit', loadPage('page/accountEdit'));
-		this.get('', loadPage('page/accounts'));
+		this.get('', function () {
+			window.location.replace('#account');
+		});
 	});
 
 	// modify sammy to ignore all form submissions
@@ -21,7 +28,7 @@ define(['sammy', 'util/app'], function (sammy, app) {
 		return true;
 	};
 
-	app.location.subscribe(function (newValue) {
+	URI.current.subscribe(function (newValue) {
 		if (window.location.hash != newValue) {
 			window.location.hash = newValue;
 		}
