ceaad244de59cfab539b689ccbd1739ca8666799
Elmer He <widgetrefinery+0ea3de46@gmail.com>
2013-01-13 22:54:40 -0800

    infiniteScroll binding

---
+++ ui/src/js/contrib/knockout/infiniteScroll.js
@@ -0,0 +1,73 @@
+define([
+	'jquery',
+	'knockout',
+	'jquery.visible'
+], function ($, ko) {
+
+	var InfiniteScroll = function ($elem, fetchData) {
+		this.__$elem = $elem;
+		this.__fetchData = fetchData;
+		this.busy(false);
+		this.error(false);
+	};
+	InfiniteScroll.prototype.busy = function (busy) {
+		if (arguments.length) {
+			if (busy) {
+				this.__$elem.addClass('busy');
+			} else {
+				this.__$elem.removeClass('busy');
+			}
+		}
+		return this.__$elem.hasClass('busy');
+	};
+	InfiniteScroll.prototype.error = function (error) {
+		if (arguments.length) {
+			if (error) {
+				this.__$elem.addClass('error');
+			} else {
+				this.__$elem.removeClass('error');
+			}
+		}
+		return this.__$elem.hasClass('error');
+	};
+	InfiniteScroll.prototype.enable = function (enable) {
+		if (arguments.length) {
+			if (enable) {
+				this.__$elem.show();
+			} else {
+				this.__$elem.hide();
+			}
+		}
+	};
+	InfiniteScroll.prototype.fetchData = function () {
+		if (!this.busy() && !this.error() && this.__$elem.visible().length) {
+			this.busy(true);
+			this.__fetchData();
+		}
+	};
+
+	var tgt;
+	var windowHandler = function () {
+		if (tgt) {
+			tgt.fetchData();
+		}
+	};
+	$(window).resize(windowHandler).scroll(windowHandler);
+
+	ko.bindingHandlers.infiniteScroll = {
+		init:   function (elem, value) {
+			var $elem = $(elem);
+			value = value();
+			tgt = new InfiniteScroll($elem, value.callback);
+			tgt.enable(value.enable());
+		},
+		update: function (elem, value) {
+			value = value();
+			tgt.busy(false);
+			tgt.error(value.error());
+			tgt.enable(value.enable());
+			tgt.fetchData();
+		}
+	};
+
+});
---
+++ ui/test/contrib/knockout/infiniteScroll.qunit.js
@@ -0,0 +1,71 @@
+require([
+	'jquery',
+	'knockout',
+	'contrib/knockout/infiniteScroll'
+], function ($, ko) {
+
+	module('contrib/knockout');
+
+	test('infiniteScroll', function () {
+		//setup
+		var $root = $('<div style="position:fixed;top:0;left:0" data-bind="infiniteScroll:{enable:enable,error:error,callback:callback}">X</div>').appendTo($('body'));
+		var invoked = 0;
+		var model = {
+			enable:   ko.observable(false),
+			error:    ko.observable(false),
+			callback: function () {
+				invoked++;
+			}
+		};
+		ko.applyBindings(model, $root[0]);
+		//test binding
+		equal(invoked, 0, 'invoked 0 times after setup');
+		equal($root.hasClass('busy'), false, 'dom is not marked as busy');
+		equal($root.hasClass('error'), false, 'dom is not marked as error');
+		model.enable(true);
+		equal(invoked, 1, 'invoked 1 time after enabling');
+		equal($root.hasClass('busy'), true, 'dom is marked as busy');
+		equal($root.hasClass('error'), false, 'dom is not marked as error');
+		model.error(true);
+		equal(invoked, 1, 'invoked 1 time after setting error');
+		equal($root.hasClass('busy'), false, 'dom is not marked as busy');
+		equal($root.hasClass('error'), true, 'dom is marked as error');
+		$root.css('top', '101%');
+		model.error(false);
+		equal(invoked, 1, 'invoked 1 time after clearing error and moving off screen');
+		equal($root.hasClass('busy'), false, 'dom is not marked as busy');
+		equal($root.hasClass('error'), false, 'dom is not marked as error');
+		$root.css('top', '50%');
+		$(window).trigger('scroll');
+		equal(invoked, 2, 'invoked 2 times after triggering scroll event');
+		equal($root.hasClass('busy'), true, 'dom is marked as busy');
+		equal($root.hasClass('error'), false, 'dom is not marked as error');
+		$(window).trigger('scroll');
+		equal(invoked, 2, 'invoked 2 times after triggering scroll event again');
+		equal($root.hasClass('busy'), true, 'dom is marked as busy');
+		equal($root.hasClass('error'), false, 'dom is not marked as error');
+		$root.css('top', '101%');
+		model.enable(false);
+		model.enable(true);
+		equal(invoked, 2, 'invoked 2 times after toggling enable');
+		equal($root.hasClass('busy'), false, 'dom is not marked as busy');
+		equal($root.hasClass('error'), false, 'dom is not marked as error');
+		$root.css('top', '50%');
+		$(window).trigger('resize');
+		equal(invoked, 3, 'invoked 3 times after triggering resize event');
+		equal($root.hasClass('busy'), true, 'dom is marked as busy');
+		equal($root.hasClass('error'), false, 'dom is not marked as error');
+		$(window).trigger('resize');
+		equal(invoked, 3, 'invoked 3 times after triggering resize event again');
+		equal($root.hasClass('busy'), true, 'dom is marked as busy');
+		equal($root.hasClass('error'), false, 'dom is not marked as error');
+		model.enable(false);
+		model.enable(true);
+		equal(invoked, 4, 'invoked 4 times after toggling enable');
+		equal($root.hasClass('busy'), true, 'dom is marked as busy');
+		equal($root.hasClass('error'), false, 'dom is not marked as error');
+		//cleanup
+		$root.remove();
+	});
+
+});
